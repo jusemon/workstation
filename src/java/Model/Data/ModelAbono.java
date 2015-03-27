52c8cc91c094712b1458275032a45f9056ce3d35
aprendiz <aprendiz@10.3.11.36>
2015-03-27 15:31:46 -0500

    Creacion Controller Abono, Model Abono, spIngresarAbono

--- src/java/Model/Data/ModelAbono.java
+++ src/java/Model/Data/ModelAbono.java
@@ -5,25 +5,40 @@
  */
 package Model.Data;
 
-import Model.DTO.ObjAbono;
 import Model.JDBC.ConnectionDB;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import Model.DTO.ObjAbono;
 /**
  *
  * @author Administrador
  */
 public class ModelAbono extends ConnectionDB {
-    private PreparedStatement pStmr;
+    private PreparedStatement pStmt;
 
-    public ModelAbono(PreparedStatement pStmr) {
+    public ModelAbono(){
         getConnection();
     }
     
     public boolean Add(ObjAbono _objAbono){
-        boolean objReturn=false;
-        return false;
+        boolean objReturn = false;
+        String sql = "call spIngresarAbono(?,?,?)";
+        
+        try {
+            getStmt();
+            pStmt = connection.prepareCall(sql);
+            pStmt.setDouble(1, _objAbono.getValorAbono());
+            pStmt.setString(1, _objAbono.getFechaPago());
+            pStmt.setInt(1, _objAbono.getIdCredito());
+            
+            int updateCount = pStmt.executeUpdate();
+            if (updateCount > 0) {
+                objReturn = true;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return objReturn;
     }
-    
-    
 }
