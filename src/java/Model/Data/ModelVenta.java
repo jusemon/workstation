a5313b1fa5b8d04129041db2f88cf58fee9a5f91
siete <siete@siete-PC>
2015-05-31 18:33:06 -0500

    Merge origin/master
    Conflicts:
    	src/java/Controller/ControllerCompra.java
    	web/public/js/crud.js

--- src/java/Model/Data/ModelVenta.java
+++ src/java/Model/Data/ModelVenta.java
@@ -25,19 +25,49 @@ public class ModelVenta extends ConnectionDB {
     public ModelVenta() {
         getConnection();
     }
-    
-    public boolean Add(ObjVenta _objVenta, ObjUsuario _objUsuario, List<ObjDetalleMovimiento> _listObjDetalleMovimientos){
+
+    public boolean Add(ObjVenta _objVenta, ObjUsuario _objUsuario, List<ObjDetalleMovimiento> _listObjDetalleMovimientos) {
         boolean objReturn = false;
         String sql1 = "call spIngresarVenta (?,?,?,?)";
         String sql2 = "call spIngresarDetalleVenta (?,?,?,?,?,)";
-        
-        try{
-         getStmt();
-                 
-                 
-        }
-    }
-    
-    
 
+        try {
+            getStmt();
+            connection.setAutoCommit(false);
+            pStmt = connection.prepareCall(sql1);
+            pStmt.setString(1, _objVenta.getDocumentoCliente());
+            pStmt.setString(2, _objVenta.getNombreCliente());
+            pStmt.setString(3, _objVenta.getFechaVenta());
+            pStmt.setInt(4, _objVenta.getTotalVenta());
+
+            int updadeCount = pStmt.executeUpdate();
+            if (updadeCount > 0) {
+                objReturn = true;
+                pStmt = connection.prepareCall(sql2);
+                for (ObjDetalleMovimiento _objDetalle : _listObjDetalleMovimientos) {
+                    pStmt.setInt(1, _objDetalle.getIdArticulo());
+                    pStmt.setInt(2, _objDetalle.getCantidad());
+                    pStmt.setInt(3, _objDetalle.getDescuento());
+                    pStmt.setInt(4, _objDetalle.getTotalDetalleMovimiento());
+                    pStmt.setInt(5, _objDetalle.getPrecioArticulo());
+                    if (pStmt.executeUpdate() > 0) {
+                        objReturn = true;
+                    } else {
+                        connection.rollback();
+                    }
+                }
+            } else {
+                connection.rollback();
+            }
+            connection.commit();
+        } catch (SQLException sqlE) {
+            System.out.println(sqlE.getMessage());
+            try {
+                connection.rollback();
+            } catch (Exception e) {
+                System.out.println(sqlE.getMessage());
+            }
+        }
+        return objReturn;
+    }
 }
