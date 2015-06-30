c8e85d9ac3e0d6513588f1adc6207e5c8f4a6772
Administrador <Administrador@ADSI>
2015-06-30 15:59:04 -0500

    Inscritos a seminario en proceso

---
+++ src/java/Model/DTO/ObjIncrito.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.DTO;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ObjIncrito {
+
+    private int idincrito;
+    private String documento;
+    private String nombres;
+    private String telefono;
+    private String correo;
+    private int idseminario;
+
+    public int getIdincrito() {
+        return idincrito;
+    }
+
+    public void setIdincrito(int idincrito) {
+        this.idincrito = idincrito;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getIdseminario() {
+        return idseminario;
+    }
+
+    public void setIdseminario(int idseminario) {
+        this.idseminario = idseminario;
+    }
+
+}
--- web/public/js/validaciones.js
+++ web/public/js/validaciones.js
@@ -3,27 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$.validator.addMethod("nombres", function(value, element) {
+$.validator.addMethod("nombres", function (value, element) {
     return this.optional(element) || /^[ÁÉÍÓÚáéíóúñÑa-zA-Z ]{3,15}/.test(value);
 }, 'Entre 3 y 15 letras y no se permiten números.');
 
-$.validator.addMethod("descripcionArticulo", function(value, element) {
+$.validator.addMethod("descripcionArticulo", function (value, element) {
     return this.optional(element) || /^[áéíóúÁÉÍÓÚñÑ°.,:'&quot;0-9a-zA-Z ]{3,30}/.test(value);
 }, 'Entre 3 y 30 letras, se permiten números y algunos caracteres como , y .');
 var salida;
-$.validator.addMethod("pass", function(value, element) {
+$.validator.addMethod("pass", function (value, element) {
     return this.optional(element) || /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(value);
 }, 'La contraseña debe contener al menos una letra minúscula, otra mayúscula y un número, la contraseña no debe ser menor a seis dígitos.');
 
-$.validator.addMethod("identificacion", function(value, element) {
+$.validator.addMethod("identificacion", function (value, element) {
     return this.optional(element) || /^[0-9]{5,15}/.test(value);
 }, 'Entre 5 y  15 dígitos.');
 
-$.validator.addMethod("fecha", function(value, element) {
+$.validator.addMethod("fecha", function (value, element) {
     return this.optional(element) || /^(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[0-2]).([0-9]{4})/.test(value);
 }, 'El formato de la fecha debe ser dd/mm/yyyy');
 
-$.validator.addMethod("fechaMayor", function(value, element) {
+$.validator.addMethod("fechaMayor", function (value, element) {
     var fecha = new Date();
     //var fechaSeminario = new Date(value.substr(6,4),value.substr(3,2),value.substr(0,2),value.substr(11,2),value.substr(14,2));    
     var dia, mes, año;
@@ -31,19 +31,24 @@ $.validator.addMethod("fechaMayor", function(value, element) {
     mes = parseInt(value.substr(3, 2));
     año = parseInt(value.substr(6, 4));
     if (año >= fecha.getFullYear()) {
-        if (mes >= fecha.getMonth() + 1) {
-            if (mes === fecha.getMonth() + 1) {
-                return dia > fecha.getDate();
-            }
-            else {
-                return true;
+        if (año > fecha.getFullYear()) {
+            return true;
+        }
+        else {
+            if (mes >= fecha.getMonth() + 1) {
+                if (mes === fecha.getMonth() + 1) {
+                    return dia > fecha.getDate();
+                }
+                else {
+                    return true;
+                }
             }
         }
     }
 }, 'La fecha ingresada no puede ser menor o igual a la actual.');
 
 //Validación para calcular que el cliente cuenta con por lo menos 5 años de edad
-$.validator.addMethod("edad", function(value, element) {
+$.validator.addMethod("edad", function (value, element) {
     var fecha = new Date();
     //var fechaSeminario = new Date(value.substr(6,4),value.substr(3,2),value.substr(0,2),value.substr(11,2),value.substr(14,2));    
     var dia, mes, año;
@@ -263,21 +268,55 @@ if (document.getElementById('formBeneficiario') !== null) {
     });
 }
 
-$('#miPopupCurso').on('shown.bs.modal', function() {
+$('#miPopupCurso').on('shown.bs.modal', function () {
     validationCurso.validate().resetForm();
 });
-$('#miPopupUsuario').on('shown.bs.modal', function() {
+$('#miPopupUsuario').on('shown.bs.modal', function () {
     validationUsuario.validate().resetForm();
 });
-$('#miPopupArticulo').on('shown.bs.modal', function() {
+$('#miPopupArticulo').on('shown.bs.modal', function () {
     validationArticulo.validate().resetForm();
 });
-$('#miPopupEstudiante').on('shown.bs.modal', function() {
+$('#miPopupEstudiante').on('shown.bs.modal', function () {
     validationEstudiante.validate().resetForm();
 });
-$('#miPopupBeneficiario').on('show.bs.modad', function() {
+$('#miPopupBeneficiario').on('show.bs.modad', function () {
     validationBeneficiario.validate().resetForm();
 });
 //$.validator.addMethod("nombres", function(value) {
 //    return ;
 //}, '');
+
+function validacionFechaRegistroASeminario(fechaSeminario) {
+    var fecha = new Date();
+    //var fechaSeminario = new Date(value.substr(6,4),value.substr(3,2),value.substr(0,2),value.substr(11,2),value.substr(14,2));    
+    var dia, mes, año, hora, minuto;
+    dia = parseInt(fechaSeminario.substr(0, 2));
+    mes = parseInt(fechaSeminario.substr(3, 2));
+    año = parseInt(fechaSeminario.substr(6, 4));
+    hora = parseInt(fechaSeminario.substr(11, 2));
+    minuto = parseInt(fechaSeminario.substr(14, 2));
+    if (año >= fecha.getFullYear()) {
+        if (año > fecha.getFullYear()) {
+            return true;
+        }
+        else {
+            if (mes >= fecha.getMonth() + 1) {
+                if (mes === fecha.getMonth() + 1) {
+                    if (dia > fecha.getDate()) {
+                        return true;
+                    } else if (dia === fecha.getDate()) {
+                        if (hora >= fecha.getHours()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else {
+                return true;
+            }
+        }
+    }
+}
